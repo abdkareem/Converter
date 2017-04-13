fa8ef55e068386259d243abf0595e294b86edc0b
Abdul Kareem <abd.khaja@gmail.com>
2017-04-13 16:41:46 -0700

    Add extra test cases and clean up code

--- test/converter.js
+++ test/converter.js
@@ -1,15 +1,22 @@
 var chai = require("chai");
 var expect = chai.expect;
 var converter = require("../app/converter");
+var hexToRgb = converter.hexToRgb;
 
 describe("Testing color code converter", function() {
-    describe("RGB to Hex conversion", function() {
-        it("converts basic colors", function(){
-            var silver = converter("C0C0C0");
-            //var green = converter.hexToRgb("00ff00");
-            //var blue = converter.hexToRgb("0000ff");
-            expect(silver).to.deep.equal([ 192, 192, 192 ]);
+    describe("Testing hex to rgb conversion", function() {
+        it("test gold conversion", function(){
+            var gold = hexToRgb("FFD700");
+            expect(gold).to.deep.equal([ 255, 215, 0 ]);
         });
+        it("test silver conversion", function(){
+            var silver = hexToRgb("C0C0C0");
+            expect(silver).to.deep.equal([192, 192, 192 ]);
+        });
+        it("test bronze conversion", function() {
+            var bronze = hexToRgb("8B4513");
+            expect(bronze).to.deep.equal([139, 69, 19])
+        })
     });
     /*
     describe("Hex to RGB conversion", function(){
